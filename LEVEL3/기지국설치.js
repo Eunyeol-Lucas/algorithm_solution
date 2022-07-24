6a230f8a9c62c355305816eac41c59b164fb58f8
Eunyeol <ney9083@gmail.com>
2022-07-25 00:19:07 +0900

    Solve: 기지국 설치

---
+++ LEVEL3/기지국설치.js
@@ -0,0 +1,18 @@
+function solution(n, stations, w) {
+  let answer = 0;
+  let index = 1;
+  const range = 2 * w + 1;
+  stations.forEach((x) => {
+    if (x - w > index) {
+      answer += Math.ceil((x - w - index) / range);
+    }
+    index = x + w + 1;
+  });
+  return answer + Math.ceil((n - index + 1) / range);
+}
+
+const n = 16,
+  stations = [9],
+  w = 2;
+
+console.log(solution(n, stations, w));
