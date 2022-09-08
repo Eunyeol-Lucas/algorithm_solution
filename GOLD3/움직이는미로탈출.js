ceca33a7fd6a07e85f30fd7ac8a633cd0c0f4ce8
Eunyeol <ney9083@gmail.com>
2022-09-09 01:29:29 +0900

    :sparkles: Solve:  움직이는미로탈출

--- GOLD3/움직이는미로탈출.js
+++ GOLD3/움직이는미로탈출.js
@@ -1,22 +1,50 @@
+const { start } = require("repl");
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n");
-const graph = input.map((i) => [...i]);
 
 const directList = [
-  [0, 1],
-  [1, 0],
-  [0, -1],
   [-1, 0],
-  [1, 1],
-  [1, -1],
-  [-1, 1],
+  [0, 1],
+  [0, -1],
   [-1, -1],
+  [-1, 1],
+  [0, 0],
+  [1, 0],
+  [1, -1],
+  [1, 1],
 ];
 
-function solution(graph) {}
+function solution(graph) {
+  const [targetX, targetY] = [0, 7];
+  const [startX, startY] = [7, 0];
+  const EMPTY = ".";
+  const NEWROW = "........";
+  const bt = (x, y, board, top) => {
+    if (x === targetX && y === targetY) return 1;
 
-console.log(solution(graph));
+    if (!board[x] || board[x][y] !== EMPTY) return 0;
+
+    if (x === 0) return 1;
+    if (x < top) return 1;
+
+    const nextBoard = [NEWROW, ...board.slice(0, -1)];
+    for (const [dx, dy] of directList) {
+      const [nx, ny] = [x + dx, y + dy];
+      if (
+        board[nx] &&
+        board[nx][ny] === EMPTY &&
+        bt(nx, ny, nextBoard, top + 1)
+      )
+        return 1;
+    }
+    return 0;
+  };
+  return bt(startX, startY, graph, 0);
+}
+
+console.log(solution(input));
