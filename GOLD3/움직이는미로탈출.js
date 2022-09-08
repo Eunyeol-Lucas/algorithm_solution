7f994b773728b3507e6298b5bb1935562e7bd90d
Eunyeol <ney9083@gmail.com>
2022-09-08 23:43:07 +0900

    :sparkles: 움직이는미로탈출

---
+++ GOLD3/움직이는미로탈출.js
@@ -0,0 +1,22 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+const graph = input.map((i) => [...i]);
+
+const directList = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, -1],
+];
+
+function solution(graph) {}
+
+console.log(solution(graph));
