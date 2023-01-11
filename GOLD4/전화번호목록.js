16dd9e6d4313be3a90b866beaf5828ccfff994fb
Eunyeol <ney9083@gmail.com>
2023-01-11 21:35:32 +0900

    :sparkles: Solve: 전화번호 목록

--- GOLD4/전화번호목록.js
+++ GOLD4/전화번호목록.js
@@ -1,16 +1,24 @@
+/**
+ * 유형: 정렬
+ * 풀이: 전화번호 문자열을 오름차순으로 정렬
+ * 한 번호다 다음 번호의 접두어인 경우가 존재하는지 확인
+ */
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 const T = +input[0];
 
 function solution(arr) {
-  let answer = "YES";
+  let answer = "YES",
+    previousNumberLength = 0;
   arr.sort((a, b) => a.localeCompare(b));
-  const regex = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i].startsWith(regex)) {
+  // 번호 A가 다른 번호의 접두어인 경우가 존재하면, 번호 A 바로 뒤에 A를 접두어로 가지는 번호가 오는 경우가 항상 존재
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].slice(0, previousNumberLength) === arr[i - 1]) {
       answer = "NO";
       break;
     }
+    previousNumberLength = arr[i].length;
   }
 
   return answer;
