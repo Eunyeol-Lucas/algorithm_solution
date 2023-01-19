b17a6f489c370e42c78e9ea858d54f6eed6b63a2
Eunyeol <ney9083@gmail.com>
2023-01-20 00:35:59 +0900

    :sparkles: Solve: 개똥벌레(구간합) 주석 추가

--- GOLD5/개똥벌레.js
+++ GOLD5/개똥벌레.js
@@ -10,15 +10,20 @@ function solution(n, h, arr) {
   let result = [];
   const top = Array.from({ length: H }, () => 0);
   const btm = Array.from({ length: H }, () => 0);
-  for (let i = 0; i < N; i++) {
+  for (let i = 0; i < n; i++) {
     if (i % 2 === 0) btm[+arr[i] - 1]++;
     else top[+arr[i] - 1]++;
   }
+  // 구간합을 계산하기 위해 구간 별로 몇개의 장애물이 존재하는지 계산
+  // 4번째 구간 -> 높이 4 이상 석순, 3번째 구간 -> 높이 3 이상 석순,,,
   for (let i = h - 2; i > -1; i--) {
+    console.log(i);
     btm[i] += btm[i + 1];
     top[i] += top[i + 1];
   }
-  const down = btm.reverse();
+  
+  // top, button을 위치시키기 위해 한쪽의 석순을 뒤집음
+  const down = btm.reverse(); 
   for (let i = 0; i < down.length; i++) {
     result.push(down[i] + top[i]);
   }
