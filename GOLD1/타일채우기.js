81f94a31aa571f86fd7774d129c881a34b5fe4f1
Eunyeol <ney9083@gmail.com>
2023-01-25 23:05:53 +0900

    :sparkles: Solve: 타일채우기

--- GOLD1/타일채우기.js
+++ GOLD1/타일채우기.js
@@ -1,3 +1,7 @@
+/**
+ * 유형: 다이나믹 프로그래밍
+ */
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs")
   .readFileSync(filePath)
@@ -5,15 +9,33 @@ const input = require("fs")
   .trim()
   .split("\n");
 const T = +input[0];
-function solution(n) {
-  let answer = 0;
-  if (n === 1) return 1;
-  if (n === 2) return 5;
-}
+
+/**
+ * function 만들었을 때: 12204kb	116ms
+ * function 안만들었을 때: 9632kb	204ms
+ */
+  const dp = Array.from({ length: 30 }, () => 0);
+  dp[1] = 1;
+  dp[2] = 5;
+  for (let i = 3; i <= 30; i++) {
+    let a = 4;
+    let b = 3;
+    dp[i] += dp[i - 1] + dp[i - 2] * 4;
+    while (a <= i - 1) {
+      dp[i] = dp[i] + dp[i - a] * 3;
+      a += 2;
+    }
+    while (b <= i - 1) {
+      dp[i] = dp[i] + dp[i - b] * 2;
+      b += 2;
+    }
+    if (i % 2 === 0) dp[i] += +3;
+    else dp[i] += 2;
+  }
 
 let answer = "";
 for (let i = 1; i <= T; i++) {
   const N = +input[i];
-  answer += solution(N) + "\n";
+  answer += dp[N] + "\n";
 }
 console.log(answer);
