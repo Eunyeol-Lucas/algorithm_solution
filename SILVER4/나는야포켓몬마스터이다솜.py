f13829f6715cfb5f24d9b0397b0fdd4867130527
Eunyeol <ney9083@gmail.com>
2023-02-08 00:33:22 +0900

    :sparkles: Solve: 나는야포켓몬마스터이다솜(다른 버전)

--- SILVER4/나는야포켓몬마스터이다솜.py
+++ SILVER4/나는야포켓몬마스터이다솜.py
@@ -1,17 +1,16 @@
-import sys
-input = sys.stdin.readline
-
-pr = open(0).read().rstrip().split('\n')
-
+# dict의 프로퍼티로 이름-인덱스, 인덱스-이름 순으로 저장
+pr = open('input.txt', 'rt').read().rstrip().split('\n')
 N, M = map(int, pr[0].split())
 
-# dict의 프로퍼티로 이름-인덱스, 인덱스-이름 순으로 저장
-dict = {}
-for i in range(N):
-  name = input().rstrip()
-  dict[name] = i+1
-  dict[str(i+1)] = name
+# 이름과 순서를 쌍으로 딕셔너리에 추가
+_dic = dict(zip(pr[1:N+1], range(1, N+1)))
+answer = []
+for x in pr[N+1:]:
+  # x가 문자일 경우 포켓몬 이름이므로 숫자를, 아닐 경우 포켓몬 이름을 추가
+  answer.append(str(_dic[x]) if x.isalpha() else pr[int(x)])
+print('\n'.join(answer))
 
-for _ in range(M):
-  m = input().rstrip()
-  print(dict[m])
+
+# dict = dict(zip(pr[1:N+1], range(1, N+1)))
+
+# print('\n'.join(map(lambda x: str(dict[x]) if x.isalpha() else pr[int(x)], pr[N+1:])))
