c2fe334992d7b7b93302aa8ea85639ebf8dbade8
Eunyeol <ney9083@gmail.com>
2023-02-12 15:49:07 +0900

    :sparkles: Solve: 환승

--- GOLD2/환승.js
+++ GOLD2/환승.js
@@ -1,3 +1,9 @@
+/**
+ * 유형: 너비 우선 탐색
+ * 모든 데이터를 인접 리스트 방식으로 저장하면 메로리 초과가 발생
+ * 메모리를 효율적으로 사용하는 것이 이 문제의 핵심인듯
+ */
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
@@ -29,22 +35,50 @@ class Queue {
 }
 
 function solution(n, k, m, hyperTubes) {
-  const graph = Array.from({ length: N + M + 1 }, () => new Array());
+  if (n === 1) return 1;
+
+  // 각 역이 몇 번째 하이퍼 튜브를 지나가는 지 저장하기 위한 배열
+  const stationList = Array.from({ length: n + 1 }, () => new Array());
+  const hyperTubeList = Array.from({ length: m + 1 }, () => new Array());
   for (let i = 0; i < m; i++) {
     const hyperTube = hyperTubes[i].split(" ").map(Number);
-    for (let j = 0; j < k; j++) {}
+    for (station of hyperTube) {
+      stationList[station].push(i + 1);
+    }
+    hyperTubeList[i + 1] = hyperTube;
   }
 
-  console.log(graph);
+  // 역 방문 체크
+  const stationVstList = Array.from({ length: n + 1 }, () => false);
+  // 하이퍼 튜브 방문 체크
+  const hyperVstList = Array.from({ length: m + 1 }, () => false);
   const queue = new Queue();
   queue.enQueue([1, 1]);
+  stationVstList[1] = true;
+
   while (queue.size) {
-    const [t, cnt] = queue.deQueue();
-    if (t === n) return cnt;
-    for (const x of graph[t]) {
-      queue.enQueue([x, cnt + 1]);
+    const [now, cnt] = queue.deQueue();
+
+    // 현재 역에서 이사용할 수 있는 하어피튜브 번호 저장
+    const nextHyperList = [];
+    for (const idx of stationList[now]) {
+      if (!hyperVstList[idx]) {
+        nextHyperList.push(idx);
+        hyperVstList[idx] = true;
+      }
+    }
+
+    for (const hyper of nextHyperList) {
+      for (const stationIdx of hyperTubeList[hyper]) {
+        if (!stationVstList[stationIdx]) {
+          if (stationIdx === n) return cnt + 1;
+          stationVstList[stationIdx] = true;
+          queue.enQueue([stationIdx, cnt + 1]);
+        }
+      }
     }
   }
+
   return -1;
 }
 
